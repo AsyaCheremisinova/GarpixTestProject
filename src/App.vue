7680afa4611eea3f09228ceef0d05dc2f5c68e66
AsyaCheremisinova <asya_cheremisinova@mail.ru>
2024-07-11 14:39:43 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Header from './components/header/Header.vue'
+</script>
+
+<template>
+  <header>
+    <Header></Header>
+  </header>
+
+</template>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="header">
+    <img alt="Vue logo" class="logo" src="/src/assets/logo_x2 1.svg" />
+  </div>
+
+</template>
+
+<style src="./headerStyle.css"></style>
