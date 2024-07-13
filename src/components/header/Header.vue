7d04abe2f8454bd44e79d0b1847df2aa28a141c9
AsyaCheremisinova <asya_cheremisinova@mail.ru>
2024-07-13 20:45:47 +0300

    adding tabs

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import HeaderTabs from './HeaderTabs.vue'
 </script>
 
 <template>
   <div class="header">
     <img alt="Vue logo" class="logo" src="/src/assets/logo_x2 1.svg" />
+    <HeaderTabs></HeaderTabs>
   </div>
 
 </template>
---
+++ src/components/header/HeaderTabs.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const activeLink = ref(null);
+
+const setActiveLink = (event) => {
+  if (activeLink.value) {
+    activeLink.value.classList.remove('active');
+  }
+  activeLink.value = event.target;
+  activeLink.value.classList.add('active');
+};
+</script>
+
+<template>
+  <nav class="menu">
+    <ul class="menu-list">
+      <li><a href="#" @click="setActiveLink">Купоны и сертификаты</a></li>
+      <li><a href="#" @click="setActiveLink">Впечатления</a></li>
+      <li><a href="#" @click="setActiveLink">Авиабилеты</a></li>
+      <li><a href="#" @click="setActiveLink">Ж/д билеты</a></li>
+      <li><a href="#" @click="setActiveLink">Отели</a></li>    
+      <li><a href="#" @click="setActiveLink">Каршеринг</a></li>
+      <li><a href="#" @click="setActiveLink">Театры</a></li>
+      <li><a href="#" @click="setActiveLink">Страхование</a></li>
+      <li><a href="#" @click="setActiveLink">Как подключиться</a></li>
+      <li><a href="#" @click="setActiveLink">Партнеры</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style src="./headerStyle.css"></style>
